c00e1533556e1e9927815e8b120ae54bbc8c84ff
Alexandre Vieira de Souza <ragnar.sistemas@gmail.com>
2019-09-16 07:11:22 -0300

    pequenas alterações

--- src/main/java/br/com/ragnar/gerenciadortarefas/util/DateUtil.java
+++ src/main/java/br/com/ragnar/gerenciadortarefas/util/DateUtil.java
@@ -6,8 +6,10 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-public class DateUtil {
+public final class DateUtil {
 
+	private static final Locale LOCALE_PT_BR = new Locale("pt", "BR");
+	private static final String OPS_A_DATA_NAO_PODE_SER_NULA_OU_VAZIA = "Ops! A data não pode ser nula ou vazia.";
 	private static final String OPS_ALGUMA_COISA_NAO_SAIU_COMO_ESPERADO = "Ops! Alguma coisa não saiu como esperado.";
 	public static final String FORMATO_DATA_BRASILEIRO = "dd/MM/yyyy";
 
@@ -16,10 +18,10 @@ public class DateUtil {
 
 	public static Date toDate(String data) {
 		if (data == null || data.isEmpty()) {
-			throw new IllegalArgumentException("Ops! A data não pode ser nula ou vazia.");
+			throw new IllegalArgumentException(OPS_A_DATA_NAO_PODE_SER_NULA_OU_VAZIA);
 		}
 		try {
-			DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+			DateFormat formatter = new SimpleDateFormat(FORMATO_DATA_BRASILEIRO);
 			return (Date) formatter.parse(data);
 		} catch (ParseException e) {
 			throw new IllegalArgumentException(OPS_ALGUMA_COISA_NAO_SAIU_COMO_ESPERADO);
@@ -27,7 +29,7 @@ public class DateUtil {
 	}
 
 	public static Date convertBr(String date) {
-		SimpleDateFormat format = new SimpleDateFormat(FORMATO_DATA_BRASILEIRO, new Locale("pt", "BR"));
+		SimpleDateFormat format = new SimpleDateFormat(FORMATO_DATA_BRASILEIRO, LOCALE_PT_BR);
 		format.setLenient(false);
 		try {
 			return new java.util.Date(format.parse(date).getTime());
@@ -37,7 +39,7 @@ public class DateUtil {
 	}
 
 	public static String convertBr(Date date) {
-		SimpleDateFormat format = new SimpleDateFormat(FORMATO_DATA_BRASILEIRO, new Locale("pt", "BR"));
+		SimpleDateFormat format = new SimpleDateFormat(FORMATO_DATA_BRASILEIRO, LOCALE_PT_BR);
 		try {
 			return format.format(date);
 		} catch (Exception e) {
